e576a28e66fc32d5c389feca364ed568c8dd3bfb
Akash Mahmud <mahmudakash177@gmail.com>
2022-09-30 15:36:22 +0600

    catalog model defination

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,24 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
+import 'package:flutter_application_1/widgets/item_widget.dart';
+import 'package:flutter/services.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-          appBar:AppBar(
+  State<Home> createState() => _HomeState();
+}
 
-            title: Text("Catalog App"),
-          ),
-        
-        body: Center(
-            
-            child: Container(
-            child: 
-            Text("Hello flutter"),
-    )),
-    drawer: MyDrawer(),
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    try {
+      // var catalogJson =
+          // await rootBundle.loadString('assests/files/catalog.json');
+      // print(catalogJson);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(item: dummyList[index]);
+          },
+        ),
+      ),
+      drawer: MyDrawer(),
     );
   }
 }
